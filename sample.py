d50714fbec572076f8720ce7fab55092589fa381
Ashish2000L <ashishkumar28april@gmail.com>
2020-05-02 15:56:56 +0530

    updated faster method
    added pickle to the classifier

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 from nltk.corpus import stopwords
 from statistics import mode
+import pickle
 import numpy as np
 import time
 
@@ -19,9 +20,11 @@ dp2=pd.read_csv('negative.txt', sep='\t', names=['likes', 'text'])
 concat=pd.concat([dp1,dp2])
 
 Y=concat.likes
+'''
 #vecterizing the data
 stops=set(stopwords.words('english'))
 vect=TfidfVectorizer(use_idf=True, lowercase=True, strip_accents='ascii',stop_words=stops)
+'''
 X=vect.fit_transform(concat.text)
 
 #spliting the data
@@ -31,39 +34,59 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=5)
 clf=naive_bayes.MultinomialNB()
 clf.fit(X_train,Y_train)
 
-#to check the accuracy of the process
-from sklearn.naive_bayes import GaussianNB
-
-#gnb=GaussianNB()
-clf=MultinomialNB()
+MNB_classifier=MultinomialNB()
 BNB_classifier=BernoulliNB()
 LR_classifier=LogisticRegression()
 SGDC_classifier=SGDClassifier()
-SVC_classifier=SVC()
 LSVC_classifier=LinearSVC()
-NSVC_classifier=NuSVC()
+'''
 
-print('Working on it...')
 #this is how we find accuracy
 '''gnb.fit(X_train.toarray(),Y_train)
 Y_pred_gnb=gnb.predict(X_test.toarray())
 print("Accuracy of GaussianNB: ",metrics.accuracy_score(Y_test,Y_pred_gnb)*100)'''
 
-clf.fit(X_train.toarray(),Y_train)
+#MNB_classifier.fit(X_train.toarray(),Y_train)
 
-BNB_classifier.fit(X_train.toarray(),Y_train)
-print("please Don't refresh")
-LR_classifier.fit(X_train.toarray(),Y_train)
+#pickled and loaded the MNB_classifier
+multinomial_classifier=open('multinom_clf.pickle','rb')
+MNB_classifier=pickle.load(multinomial_classifier)
+multinomial_classifier.close()
 
-SGDC_classifier.fit(X_train.toarray(),Y_train)
-print('Almost done!!!')
-LSVC_classifier.fit(X_train.toarray(),Y_train)
+#BNB_classifier.fit(X_train.toarray(),Y_train)
 
+#pickled and loaded the BNB_classifier
+Bernauli_classifier=open('bernauli_clf.pickle','rb')
+BNB_classifier=pickle.load(Bernauli_classifier)
+Bernauli_classifier.close()
+
+#LR_classifier.fit(X_train.toarray(),Y_train)
+
+#pickled and loaded the LR_classifier
+logistic_classifier=open('logistic_clf.pickle','rb')
+LR_classifier=pickle.load(logistic_classifier)
+logistic_classifier.close()
+
+#SGDC_classifier.fit(X_train.toarray(),Y_train)
+
+#pickled and loaded the SGDC_classifier
+sgdc_classifier=open('SGDC_clf.pickle','rb')
+SGDC_classifier=pickle.load(sgdc_classifier)
+sgdc_classifier.close()
+
+#LSVC_classifier.fit(X_train.toarray(),Y_train)
+
+#pickled and loaded the LSVC_classifier
+lsvc_classifier=open('LSVC_clf.pickle','rb')
+LSVC_classifier=pickle.load(lsvc_classifier)
+lsvc_classifier.close()
+
+print('lsvc comlete')
 #function to pass the string
 votes=[]
 def sentence(sent):
     movie_review_vect=vect.transform(np.array([sent]))
-    review = clf.predict(movie_review_vect)
+    review = MNB_classifier.predict(movie_review_vect)
     votes.append(review)
     review = BNB_classifier.predict(movie_review_vect)
     votes.append(review)
