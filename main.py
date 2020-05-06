5acb5eb5226b81ee7be4028a7e5980b84de8347c
Ashish2000L <ashishkumar28april@gmail.com>
2020-05-06 23:20:53 +0530

    added classifiers
    added new classifiers and updated the training and testing data

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from shortsent import sent
-from sample import sentence
+import pandas as pd
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
 from matplotlib import  pyplot as plt
 from matplotlib import style
-import nltk.text as txt
-import time
-import pandas as pd
+from sample import sentence
+import  nltk.text as txt
+from statistics import mode
+from shortsent import sent
 
 #def word():
 #    data=open('data.csv','r')
@@ -53,37 +55,97 @@ import pandas as pd
 #plt.legend()
 #plt.show()
 
+dp1=pd.read_csv('pos.csv',names=['Rev','Text'])
+dp2=pd.read_csv('neg.csv', names=['Rev','Text'])
+
+concat=pd.concat([dp1,dp2])
+
+Y=concat.Rev
+
+#vecterizing the data
+stops=set(stopwords.words('english'))
+vect=TfidfVectorizer(use_idf=True, lowercase=True, strip_accents='ascii',stop_words=stops,ngram_range=(1,3))
+
+#X=vect.fit_transform(concat.Text)
+
+
 lst=[]
+lst_neg=[]
+lt_neg={}
 lt={}
-def value(string):
+def value(string1):
+    string=str(string1)
     try:
-        file=open('C:\\Users\\pratibha\\PycharmProjects\\test\\positive.txt','r')
+        file=open('pos.txt', 'rb')
+        file_next=open('neg.txt','rb')
         for i in file:
-            lst.append(i)
+            lst.append(i.decode())
+
+        for z in file_next:
+            lst_neg.append(z.decode())
+        make_text_neg=txt.TextCollection(lst_neg)
         make_text=txt.TextCollection(lst)
         output, shortsent_time, update_time = sent(string)
-
+        print(output)
+        result,confi=sentence(output)
         for i in output.split(' '):
             if i not in lt:
                 lt[i]=make_text.tf_idf(i,output)
+
+        for j in output.split(' '):
+            if j not in lt_neg:
+                lt_neg[j]=make_text_neg.tf_idf(j,output)
     except Exception as ec:
+        print('hello')
         print(ec)
     else:
-        return lt,string
-
-val,user_string=value(str(input("Enter your text: ")))
-file=open('testing.csv','w')
+        return result,confi
+x=str(input("Enter your text: "))
+result,confi=value(x)
+file=open('testing_pos.csv','w')
+file_neg=open('testing_neg.csv','w')
 for z in lt:
     file.write(z)
     file.write(',')
-    file.write(str(val.get(z)))
+    file.write(str(lt.get(z)))
     file.write('\n')
+
+for k in lt_neg:
+    file_neg.write(k)
+    file_neg.write(',')
+    file_neg.write(str(lt.get(k)))
+    file_neg.write('\n')
 file.close()
-items=[x[1] for x in lt.items()]
-style.use('ggplot')
-plt.plot(list(lt.keys()),items,'r',linewidth=1)
-plt.title(f"Tf-idf of string:\n{user_string}")
-plt.ylabel('tf_idf value')
-plt.xlabel('words ')
-#plt.legend()
-plt.show()
+file_neg.close()
+#items=[x[1] for x in lt.items()]
+#style.use('ggplot')
+#plt.plot(list(lt.keys()),items,'r',linewidth=1)
+#plt.title(f"Tf-idf of string:\n{x}")
+#plt.ylabel('tf_idf value')
+#plt.xlabel('words ')
+##plt.legend()
+#plt.show()
+#print((result,confi))
+pf_pos=pd.read_csv('testing_pos.csv', names=['words','tf_idf'])
+pf_neg=pd.read_csv('testing_neg.csv',names=['words','tf_idf'])
+#test = pf_pos.sort_values('tf_idf', ascending=False)#to short the values
+initial=len(pf_pos['words'])
+cf=pf_pos[pf_pos.tf_idf!=0.0]
+print('percentage matching: ',((len(cf['words'])/initial)*100))
+pos_sum=sum(pf_pos['tf_idf'])
+neg_sum=sum(pf_neg['tf_idf'])
+if pos_sum > neg_sum:
+    print('Matching to the positive: ',pos_sum)
+    result.append(1)
+else:
+    print('Matching to the negative: ',neg_sum)
+    result.append(0)
+
+res=mode(result)
+confidance=result.count(res)/len(result)
+if res==1:
+    res='Positive'
+else:
+    res='Negative'
+
+print((res,confidance))
--- sample.py
+++ sample.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import naive_bayes
+from sklearn import naive_bayes, metrics
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.svm import  LinearSVC, NuSVC
+from sklearn.svm import LinearSVC, NuSVC, SVC
 from nltk.corpus import stopwords
 from statistics import mode
 import pickle
@@ -13,16 +13,16 @@ import time
 
 
 #get the detailed data
-concat=pd.read_csv('data.csv')
-#dp2=pd.read_csv('negative.txt', sep='\t', names=['likes', 'text'])
+dp1=pd.read_csv('pos.csv',names=['Rev','Text'])
+dp2=pd.read_csv('neg.csv',names=['Rev','Text'])
 
-#concat=pd.concat([dp1,dp2])
+concat=pd.concat([dp1,dp2])
 
 Y=concat.Rev
 
 #vecterizing the data
 stops=set(stopwords.words('english'))
-vect=TfidfVectorizer(use_idf=True, lowercase=True, strip_accents='ascii',stop_words=stops,ngram_range=(1,3),)
+vect=TfidfVectorizer(use_idf=True,lowercase=True, strip_accents='ascii',stop_words=stops,ngram_range=(1,3),)
 
 X=vect.fit_transform(concat.Text)
 
@@ -30,9 +30,7 @@ X=vect.fit_transform(concat.Text)
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=5)
 
 #training using MultinomialNB
-clf=naive_bayes.MultinomialNB()
-clf.fit(X_train,Y_train)
-
+gnb=GaussianNB()
 MNB_classifier=MultinomialNB()
 BNB_classifier=BernoulliNB()
 LR_classifier=LogisticRegression()
@@ -41,11 +39,13 @@ LSVC_classifier=LinearSVC()
 
 
 #this is how we find accuracy
-'''gnb.fit(X_train.toarray(),Y_train)
+gnb.fit(X_train.toarray(),Y_train)
 Y_pred_gnb=gnb.predict(X_test.toarray())
-print("Accuracy of GaussianNB: ",metrics.accuracy_score(Y_test,Y_pred_gnb)*100)'''
+#print("Accuracy of GaussianNB: ",metrics.accuracy_score(Y_test,Y_pred_gnb)*100)
 
 MNB_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_MNB=MNB_classifier.predict(X_test.toarray())
+#print("Accuracy of MNB_classifier: ",metrics.accuracy_score(Y_test,Y_pred_MNB)*100)
 
 #pickled and loaded the MNB_classifier
 multinomial_classifier=open('multinom_clf.pickle','wb')
@@ -53,6 +53,8 @@ pickle.dump(MultinomialNB,multinomial_classifier)
 multinomial_classifier.close()
 
 BNB_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_BNB=BNB_classifier.predict(X_test.toarray())
+#print("Accuracy of BNB_classifier: ",metrics.accuracy_score(Y_test,Y_pred_BNB)*100)
 
 #pickled and loaded the BNB_classifier
 Bernauli_classifier=open('bernauli_clf.pickle','wb')
@@ -60,6 +62,8 @@ pickle.dump(BNB_classifier,Bernauli_classifier)
 Bernauli_classifier.close()
 
 LR_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_LR=LR_classifier.predict(X_test.toarray())
+#print("Accuracy of LR_classifier: ",metrics.accuracy_score(Y_test,Y_pred_LR)*100)
 
 #pickled and loaded the LR_classifier
 logistic_classifier=open('logistic_clf.pickle','wb')
@@ -67,6 +71,8 @@ pickle.dump(LR_classifier,logistic_classifier)
 logistic_classifier.close()
 
 SGDC_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_SGDC=SGDC_classifier.predict(X_test.toarray())
+#print("Accuracy of SGDC_classifier: ",metrics.accuracy_score(Y_test,Y_pred_SGDC)*100)
 
 #pickled and loaded the SGDC_classifier
 sgdc_classifier=open('SGDC_clf.pickle','wb')
@@ -74,26 +80,43 @@ pickle.dump(SGDC_classifier,sgdc_classifier)
 sgdc_classifier.close()
 
 LSVC_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_LSVC=LSVC_classifier.predict(X_test.toarray())
+#print("Accuracy of LSVC_classifier: ",metrics.accuracy_score(Y_test,Y_pred_LSVC)*100)
 
 #pickled and loaded the LSVC_classifier
 lsvc_classifier=open('LSVC_clf.pickle','wb')
 pickle.dump(LSVC_classifier,lsvc_classifier)
 lsvc_classifier.close()
 
+NuSVC_classifier=NuSVC()
+NuSVC_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_NuSVC=NuSVC_classifier.predict(X_test.toarray())
+#print("Accuracy of NuSVC_classifier: ",metrics.accuracy_score(Y_test,Y_pred_NuSVC)*100)
+
+SVC_classifier=SVC()
+SVC_classifier.fit(X_train.toarray(),Y_train)
+Y_pred_SVC=SVC_classifier.predict(X_test.toarray())
+#print("Accuracy of SVC_classifier: ",metrics.accuracy_score(Y_test,Y_pred_SVC)*100)
 #function to pass the string
 votes=[]
 def sentence(sent):
     sample_start = time.time()
-    vectors=vect.transform(np.array([sent]))
-    review = MNB_classifier.predict(vectors)
+    vectors=vect.transform(np.array([sent]))#np.array([sent])
+    review=gnb.predict(vectors.toarray())
     votes.append(review)
-    review = BNB_classifier.predict(vectors)
+    review=SVC_classifier.predict(vectors.toarray())
     votes.append(review)
-    review = LR_classifier.predict(vectors)
+    review=NuSVC_classifier.predict(vectors.toarray())
     votes.append(review)
-    review = SGDC_classifier.predict(vectors)
+    review = MNB_classifier.predict(vectors.toarray())
     votes.append(review)
-    review = LSVC_classifier.predict(vectors)
+    review = BNB_classifier.predict(vectors.toarray())
+    votes.append(review)
+    review = LR_classifier.predict(vectors.toarray())
+    votes.append(review)
+    review = SGDC_classifier.predict(vectors.toarray())
+    votes.append(review)
+    review = LSVC_classifier.predict(vectors.toarray())
     votes.append(review)
     list = []
     for i in votes:
@@ -101,12 +124,9 @@ def sentence(sent):
     res = mode(list)
     num = list.count(res)
     conf = num / len(list)
-    if res == 1:
-        res = 'Positive'
-    elif res == 0:
-        res = 'Negative'
-    sample_time = time.time() - sample_start
-    return res, conf*100,sample_time
-
-
-print(sentence('corona is dangerous'))
+    #if res == 1:
+    #    res = 'Positive'
+    #elif res == 0:
+    #    res = 'Negative'
+    #sample_time = time.time() - sample_start
+    return list,conf*100
