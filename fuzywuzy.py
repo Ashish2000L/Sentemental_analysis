0865b38cd898b9deb6eaa6917079084579ac00cb
Ashish2000L <ashishkumar28april@gmail.com>
2020-05-19 18:10:03 +0530

    added fuzywuzy.py

---
+++ fuzywuzy.py
@@ -0,0 +1,19 @@
+from fuzzywuzzy import fuzz
+from ploting_data import plots
+def str_cmp(inpts):
+    file=open('pos.txt','r')
+    file1=open('fuzywuzy_data.csv','w')
+    file1.write('ratio,partial_ratio,token_sort_ratio,token_set_ratio,none \n')
+    for i in file:
+        file1.write(str(fuzz.ratio(inpts.lower(),i.lower())))
+        file1.write(',')
+        file1.write(str(fuzz.partial_ratio(inpts.lower(),i.lower())))
+        file1.write(',')
+        file1.write(str(fuzz.token_sort_ratio(inpts,i)))
+        file1.write(',')
+        file1.write(str(fuzz.token_set_ratio(inpts,i)))
+        file1.write('\n')
+    file1.close()
+    file.close()
+
+    plots()
